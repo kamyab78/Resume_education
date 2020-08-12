c4424ef607d4c2ea2623b32b3075d14e83e5c382
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-12 04:36:33 +0430

    Add full width

--- resume/src/Landing/Landing.js
+++ resume/src/Landing/Landing.js
@@ -72,7 +72,7 @@ class Landing extends Component {
 
                                 </div></div>
                             <div id='about_page' className='about'>
-
+jbhvjgc
                             </div>
                             <p id='education_page' className='education'> Education  ScrolSome text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus g
                             ggggggggggggggggggggggggggggggggggggggggggggggggysdcyudsgcuysgdcyusgdcuygsyudcgusydgcSome text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus g
