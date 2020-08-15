33a6b4f1c122fdf1bfb2530d65b164bae0a17804
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-16 03:50:01 +0430

    Add scroll

--- resume/src/Landing/Landing.js
+++ resume/src/Landing/Landing.js
@@ -103,8 +103,46 @@ class Landing extends Component {
                 $(".circle_three").css("border", "4px solid gray");
                 $(".triangle2").css("border-right", "30px solid transparent");
               });
-             
+              $(window).scroll(function(){
+                var height = $(window).scrollTop();
+                console.log(height)
+                if(height>=3621.875){
+                    setTimeout(function () {
+                        $('#title1').fadeIn('slow')
+                    }, 200);
+                    setTimeout(function () {
+                        $('#row_one').fadeIn('slow')
+                    }, 400);
+                    setTimeout(function () {
+                        $('#row_two').fadeIn('slow')
+                    }, 600);
+                    setTimeout(function () {
+                        $('#row_three').fadeIn('slow')
+                    }, 800);
+                    setTimeout(function () {
+                        $('#title2').fadeIn('slow')
+                    }, 1000);
+                    setTimeout(function () {
+                        $('#row_four').fadeIn('slow')
+                    }, 1200);
+                    setTimeout(function () {
+                        $('#row_five').fadeIn('slow')
+                    }, 1400);
+                    setTimeout(function () {
+                        $('#row_six').fadeIn('slow')
+                    }, 1600);
+                    setTimeout(function () {
+                        $('#row_seven').fadeIn('slow')
+                    }, 1800);
+                    setTimeout(function () {
+                        $('#row_eight').fadeIn('slow')
+                    }, 2000);
+                }
+              })
         })
+    }
+    scroll_skill(){
+        console.log(1212121212)
     }
     render() {
         return (
@@ -213,7 +251,7 @@ class Landing extends Component {
                                 
                             </div>
                             </div>
-                            <div id='skill_page' className='skill'>
+                            <div id='skill_page' className='skill' onScroll={this.scroll_skill} >
 
                                 <h1 style={{ borderBottomColor: 'black', borderBottomStyle: 'solid', marginLeft: '100px', paddingTop: '120px ', textAlign: 'center', width: '85%' }}>Skills</h1>
                                 <h1 style={{ fontSize: '25px', marginLeft: '160px', marginTop: '20px', display: 'none' }} id='title1'>Progamming Language</h1>
