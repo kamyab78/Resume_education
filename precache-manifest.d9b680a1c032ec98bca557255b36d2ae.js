80bddd3c0fa2d68be8c34437d66a01f91c85ba39
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-21 20:17:28 +0430

    Updates

---
+++ precache-manifest.d9b680a1c032ec98bca557255b36d2ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9abc4f07357876ec5709d35c651789c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "c7aa58c2aa525e6bcb83",
+    "url": "/resume/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "263154fb694fda7eb937",
+    "url": "/resume/static/css/main.c8bfa6de.chunk.css"
+  },
+  {
+    "revision": "c7aa58c2aa525e6bcb83",
+    "url": "/resume/static/js/2.e7f874ac.chunk.js"
+  },
+  {
+    "revision": "6d1f2c54df40d51a086cb704d80d6d60",
+    "url": "/resume/static/js/2.e7f874ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263154fb694fda7eb937",
+    "url": "/resume/static/js/main.f88c2f6a.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  },
+  {
+    "revision": "a868678ed693a95ca673c20fdaebeaa4",
+    "url": "/resume/static/media/Phone-icon.a868678e.png"
+  },
+  {
+    "revision": "e708064ab79f4b2c151b56102d207edd",
+    "url": "/resume/static/media/Skype-icon.e708064a.png"
+  },
+  {
+    "revision": "3945c6c36eaa036d407b362f6c6e27ac",
+    "url": "/resume/static/media/about_back.3945c6c3.jpg"
+  },
+  {
+    "revision": "0f6d7551a0c01973178d32c0ae938f29",
+    "url": "/resume/static/media/header.0f6d7551.png"
+  }
+]);
