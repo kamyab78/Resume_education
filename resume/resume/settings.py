f3a0c83a3544c9120f214541cad1935aa3e84e88
kamyab78 <amirhosseinghassemi78@yahoo.com>
2020-08-21 21:25:18 +0430

    Fix database

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -106,12 +106,8 @@ WSGI_APPLICATION = 'resume.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'resume',
-        'USER': 'admin',
-        'PASSWORD': 'resume',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
